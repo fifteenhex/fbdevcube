62ad5495aa9ffbf726e5a9de88f8826e121b8918
Daniel Palmer <daniel@thingy.jp>
2025-12-09 21:18:04 +0900

    Bits to get the framebuffer parameters from fbdev

--- fbdevcube.c
+++ fbdevcube.c
@@ -1,3 +1,4 @@
+#include <linux/fb.h>
 #define S3L_PIXEL_FUNCTION	fbdevcube_pixel_func
 #define S3L_MAX_PIXELS		(512 * 512)
 #include "small3dlib/small3dlib.h"
@@ -9,7 +10,26 @@ static inline void fbdevcube_pixel_func(S3L_PixelInfo *p)
 
 int main(int argc, char **argv, char **envp)
 {
+	struct fb_var_screeninfo vscrinfo;
+	const char *fbdev_path = "/dev/fb0";
+	int fbfd, ret;
+
 	printf("fbdevcube\n");
+
+	fbfd = open(fbdev_path, O_WRONLY);
+	if (fbfd < 0) {
+		printf("failed to open fbdev %s: %d\n", fbdev_path, fbfd);
+		return 1;
+	}
+
+	ret = ioctl(fbfd, FBIOGET_VSCREENINFO, &vscrinfo);
+	if (ret) {
+		printf("failed to get var screeninfo: %d\n", ret);
+		return 1;
+	}
+
+	printf("framebuffer is %d x %d @ %d bpp\n",
+		vscrinfo.xres, vscrinfo.yres, vscrinfo.bits_per_pixel);
 
 	return 0;
 }
